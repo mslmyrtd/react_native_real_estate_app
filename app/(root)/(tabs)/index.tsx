f82b0ddaaec03d57606c2d5009d7ebbb7aa25c74
muslumfront <muslum@nssgames.com>
2025-01-22 22:57:37 +0300

    chore: Add GlobalProvider for global state management profile page handled

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,10 +10,6 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Link href="/sign-in" className="mb-10" style={{marginBottom :10, backgroundColor:"red" }}  >Sign</Link>
-      <Link href="/profile" style={{marginBottom :10}} >Profile</Link>
-      <Link href="/explore" style={{marginBottom :10}}>Explore</Link>
-      <Link href="/properties/1" style={{marginBottom :10}}>Properties</Link>
       <Text className="font-extrabold">Heloooo</Text>
     </View>
   );
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,30 +1,66 @@
-import { View, Text, ScrollView,Image,TouchableOpacity } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import images from '@/constants/images'
-import icons from '@/constants/icons'
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import { login } from "@/lib/appwrite";
+import { Redirect } from "expo-router";
+import { useGlobalContext } from "@/lib/global-provider";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 
 const SignIn = () => {
-  const handleLogin=()=>{
-    console.log("Login")
-  }
+  const { refetch, loading, isLogged } = useGlobalContext();
+
+  if (!loading && isLogged) return <Redirect href="/" />;
+
+  const handleLogin = async () => {
+    const result = await login();
+    if (result) {
+      refetch();
+    } else {
+      Alert.alert("Error", "Failed to login");
+    }
+  };
+
   return (
-    <SafeAreaView className='bg-white h-full mt-3'>
-      <ScrollView contentContainerClassName='h-full'>
-        <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain' />
-        <View className='px-10'>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+
+        <View className="px-10">
           <Text className="text-base text-center uppercase font-rubik text-black-200">
             Welcome To Real Scout
           </Text>
+
           <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
             Let's Get You Closer To {"\n"}
             <Text className="text-primary-300">Your Ideal Home</Text>
           </Text>
+
           <Text className="text-lg font-rubik text-black-200 text-center mt-12">
             Login to Real Scout with Google
           </Text>
-          <TouchableOpacity onPress={handleLogin} className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5">
-          <View className="flex flex-row items-center justify-center">
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+          >
+            <View className="flex flex-row items-center justify-center">
               <Image
                 source={icons.google}
                 className="w-5 h-5"
@@ -33,12 +69,12 @@ const SignIn = () => {
               <Text className="text-lg font-rubik-medium text-black-300 ml-2">
                 Continue with Google
               </Text>
-            </View>          
+            </View>
           </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
